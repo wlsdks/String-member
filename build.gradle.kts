d31ab47deea5717cc74932aed25b0490c4a3715e
wlsdks <wlsdks12@naver.com>
2024-04-27 18:07:23 +0900

    #1 feat: gradle에 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,14 +26,27 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// kotlin
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// spring
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// spring security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// db set
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("org.postgresql:postgresql")
+
+	// dev tools
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
