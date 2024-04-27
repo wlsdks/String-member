c8e500056bb672820588bf4bf16a5e668b7b0af1
wlsdks <wlsdks12@naver.com>
2024-04-27 21:58:34 +0900

    #5. fix: yml파일이름 수정, jjwt 의존성 버전 변경(신버전 래퍼런스 부족)

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,9 +42,12 @@ dependencies {
 
 	// spring security, jwt
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+//	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+//	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+//	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
 	// db set
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
