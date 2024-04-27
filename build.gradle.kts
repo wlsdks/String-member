c55a92ed54fd16b54282862a439cff8444f2517f
wlsdks <wlsdks12@naver.com>
2024-04-27 18:53:47 +0900

    #1 feat: jpa 로깅을 위한 p6spy 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,12 +47,15 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	runtimeOnly("org.postgresql:postgresql")
 
-	// jpa, querydsl
+	// jpa, querydsl, p6spy
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+
 	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 	kapt("jakarta.annotation:jakarta.annotation-api")
 	kapt("jakarta.persistence:jakarta.persistence-api")
+
+	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
 
 	// dev tools
 	compileOnly("org.projectlombok:lombok")
