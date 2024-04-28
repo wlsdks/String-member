1d9bbd8aabbe08c0076fdf7933254de88f3fd8e5
wlsdks <wlsdks12@naver.com>
2024-04-28 22:41:05 +0900

    #4 feat: 코틀린 로깅을 위한 펙토리 메서드 작성

--- src/main/kotlin/com/tony/string/StringApplication.kt
+++ src/main/kotlin/com/tony/string/StringApplication.kt
@@ -1,8 +1,11 @@
 package com.tony.string
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+inline fun <reified T> T.logger() = LoggerFactory.getLogger(T::class.java)!!
 
 @EnableJpaAuditing
 @SpringBootApplication
