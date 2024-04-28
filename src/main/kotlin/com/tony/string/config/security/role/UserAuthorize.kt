0181024dbd59e4633fc4e17b8b8d8cdd22608e80
wlsdks <wlsdks12@naver.com>
2024-04-28 22:47:34 +0900

    #7 feat: 커스텀 어노테이션 작성 (권한에 따른 코드 호출 제어)

--- src/main/kotlin/com/tony/string/config/security/role/UserAuthorize.kt
+++ src/main/kotlin/com/tony/string/config/security/role/UserAuthorize.kt
@@ -4,5 +4,5 @@ import org.springframework.security.access.prepost.PreAuthorize
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasRole('ROLE_USER')")
 annotation class UserAuthorize()
