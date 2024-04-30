99749ab7a5299ce1af840bae9e265f802312566a
wlsdks <wlsdks12@naver.com>
2024-04-30 21:45:26 +0900

    #9. jwt 인증관련 리포지토리 코드 작성

--- src/main/kotlin/com/tony/string/repository/MemberRepository.kt
+++ src/main/kotlin/com/tony/string/repository/MemberRepository.kt
@@ -1,11 +1,15 @@
 package com.tony.string.repository
 
 import com.tony.string.config.security.dto.MemberStatus
-import com.tony.string.domain.Member
+import com.tony.string.domain.MemberEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface MemberRepository : JpaRepository<Member, Long> {
-    fun findMemberByUsername(username: String?) : Member
+interface MemberRepository : JpaRepository<MemberEntity, Long> {
+    fun findMemberByUsername(username: String?) : MemberEntity
     fun existsMemberByEmailAndStatus(email: String, active: MemberStatus): Boolean
-    fun findMemberByEmail(email: String): Member
+    fun findMemberByEmail(email: String): MemberEntity
+
+    // todo: 이건 querydsl로 처리해야할듯
+    fun deactivateMember(memberId: Long): Long
+    fun findMemberByIdAndStatus(memberId: Long, active: MemberStatus): MemberEntity
 }
