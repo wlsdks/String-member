df36566f723ad290f1fefd11abe4940367b011f6
wlsdks <wlsdks12@naver.com>
2024-04-30 22:03:49 +0900

    #9 fix: 기존 repository 코드 수정

--- src/main/kotlin/com/tony/string/repository/JwtRepository.kt
+++ src/main/kotlin/com/tony/string/repository/JwtRepository.kt
@@ -4,6 +4,9 @@ import com.tony.string.domain.JwtEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface JwtRepository : JpaRepository<JwtEntity, Long> {
+
     fun deleteJwtEntityByMemberId(memberId: Long)
+
     fun findJwtEntityByMemberIdAndRefreshToken(memberId: Long, refreshToken: String): JwtEntity
+
 }
--- src/main/kotlin/com/tony/string/repository/MemberRepository.kt
+++ src/main/kotlin/com/tony/string/repository/MemberRepository.kt
@@ -5,11 +5,13 @@ import com.tony.string.domain.MemberEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository : JpaRepository<MemberEntity, Long> {
+
     fun findMemberByUsername(username: String?) : MemberEntity
+
     fun existsMemberByEmailAndStatus(email: String, active: MemberStatus): Boolean
+
     fun findMemberByEmail(email: String): MemberEntity
 
-    // todo: 이건 querydsl로 처리해야할듯
-    fun deactivateMember(memberId: Long): Long
     fun findMemberByIdAndStatus(memberId: Long, active: MemberStatus): MemberEntity
+
 }
