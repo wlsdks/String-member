83d17ef77d4e7d85839357d0ee51c05b60d8e6ec
wlsdks <wlsdks12@naver.com>
2024-04-30 23:23:50 +0900

    #9 feat: mvc 레이어에서 사용할 시큐리티 유틸 클래스 작성, 시큐리티 유틸관련 에러코드 추가

--- src/main/kotlin/com/tony/string/exception/ErrorCode.kt
+++ src/main/kotlin/com/tony/string/exception/ErrorCode.kt
@@ -10,17 +10,21 @@ enum class ErrorCode(
     EXPIRED_TOKEN("EXPIRED_TOKEN", "Expired JWT token."),
 
     // 스프링 시큐리티 예외
-    ACCESS_DENIED("ACCESS_DENIED", "Access is denied."),
-    AUTHENTICATION_FAILED("AUTHENTICATION_FAILED", "Authentication failed."),
+    ACCESS_DENIED("ACCESS_DENIED", "접근 권한이 없습니다. (security roles)"),
+    AUTHENTICATION_FAILED("AUTHENTICATION_FAILED", "인증에 실패하였습니다. (security authentication)"),
     FILTER_USERNAME_PASSWORD_AUTHENTICATION_TOKEN_EXCEPTION(
         "FILTER_USERNAME_PASSWORD_AUTHENTICATION_TOKEN_EXCEPTION",
         "UsernamePasswordAuthenticationToken Exception occurred."
     ),
+    AUTHENTICATION_NOT_FOUND("AUTHENTICATION_NOT_FOUND", "인증 정보를 찾을 수 없습니다."),
+    UNAUTHENTICATED("UNAUTHENTICATED", "사용자가 인증되지 않았습니다."),
+    PRINCIPAL_NOT_CORRECT_TYPE("PRINCIPAL_NOT_CORRECT_TYPE", "시큐리티의 Principle 타입이 JwtMemberInfoDTO 타입이 아닙니다."),
 
     // 기본 예외, 런타임 예외
-    INTERNAL_ERROR("INTERNAL_ERROR", "Internal server error."),
+    INTERNAL_ERROR("INTERNAL_ERROR", "Internal server error occurred."),
     RUNTIME_EXCEPTION("RUNTIME_EXCEPTION", "Unexpected runtime error."),
     IO_EXCEPTION("IO_EXCEPTION", "I/O error occurred."),
+
 
     // 커스텀 예외 (생길때마다 추가)
 
