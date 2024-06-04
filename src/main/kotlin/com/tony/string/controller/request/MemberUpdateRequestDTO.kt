25d0429e2658313b1b6165db05368d023fd315ad
wlsdks <wlsdks12@naver.com>
2024-06-05 12:34:29 +1242

    feat: 유저정보 업데이트 DTO에 validation 추가

--- src/main/kotlin/com/tony/string/controller/request/MemberUpdateRequestDTO.kt
+++ src/main/kotlin/com/tony/string/controller/request/MemberUpdateRequestDTO.kt
@@ -1,11 +1,17 @@
 package com.tony.string.controller.request
 
+import jakarta.validation.constraints.NotBlank
 import java.time.LocalDateTime
 
 data class MemberUpdateRequestDTO(
     val id: Long?,
+
+    @field:NotBlank(message = "유저 이름은 필수입니다.")
     val username: String,
+
+    @field:NotBlank(message = "이메일은 필수입니다.")
     val email: String,
+
     val password: String,
     val information: String?,
     val profileImageUrl: String?,
