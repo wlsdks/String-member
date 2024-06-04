824f0b3be0b06024fdd348dbbb36447ddd628653
wlsdks <wlsdks12@naver.com>
2024-06-04 22:03:23 +0900

    feat: 서비스 레이어 작성

---
+++ src/main/kotlin/com/tony/string/service/LikeService.kt
@@ -0,0 +1,4 @@
+package com.tony.string.service
+
+interface LikeService {
+}
---
+++ src/main/kotlin/com/tony/string/service/impl/LikeServiceImpl.kt
@@ -0,0 +1,9 @@
+package com.tony.string.service.impl
+
+import com.tony.string.service.LikeService
+import org.springframework.stereotype.Service
+
+@Service
+class LikeServiceImpl : LikeService {
+
+}
