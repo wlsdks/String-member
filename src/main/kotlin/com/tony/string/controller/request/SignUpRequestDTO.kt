9bc71b8d6b3a0932b34b79564d2aa6894c5ddc85
wlsdks <wlsdks12@naver.com>
2024-06-06 15:21:53 +1603

    feat: 회원가입 요청  DTO validation 추가

--- src/main/kotlin/com/tony/string/controller/request/SignUpRequestDTO.kt
+++ src/main/kotlin/com/tony/string/controller/request/SignUpRequestDTO.kt
@@ -2,6 +2,7 @@ package com.tony.string.controller.request
 
 import com.tony.string.config.security.dto.MemberStatus
 import com.tony.string.domain.Member
+import jakarta.validation.constraints.NotBlank
 import java.time.LocalDateTime
 
 /**
@@ -9,10 +10,19 @@ import java.time.LocalDateTime
  */
 data class SignUpRequestDTO(
     val id: Long?,
+
+    @field:NotBlank(message = "유저 이름은 필수입니다.")
     val username: String,
+
+    @field:NotBlank(message = "닉네임은 필수입니다.")
     val nickname: String,
+
+    @field:NotBlank(message = "이메일은 필수입니다.")
     val email: String,
+
+    @field:NotBlank(message = "비밀번호는 필수입니다.")
     val password: String,
+
     val information: String?,
     val profileImageUrl: String?,
     val location: String?,
